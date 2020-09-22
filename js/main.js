d09d076a5f1b4ce4078068a869e03680d2e55543
Moustafa Mohsen <moustafamohsen1@gmail.com>
2020-09-22 12:29:58 +0200

    autocomplete add and remove waypoint js

--- js/main.js
+++ js/main.js
@@ -154,50 +154,55 @@ $(document).ready(function () {
 
 /////////////////////////////////////////////////////////////////
 
+map
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 6,
-      center: {
-          lat: 26.8206,
-          lng: 30.8025
-      } //Egypt.
+  this.map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: {
+      lat: 26.8206,
+      lng: 30.8025
+    } //Egypt.
   });
 
-  var directionsService = new google.maps.DirectionsService;
-  var directionsRenderer = new google.maps.DirectionsRenderer({
-      draggable: true,
-      map: map,
-      panel: document.getElementById('right-panel')
+  this.directionsService = new google.maps.DirectionsService;
+  this.directionsRenderer = new google.maps.DirectionsRenderer({
+    draggable: true,
+    map: map,
+    panel: document.getElementById('right-panel')
   });
 
-  directionsRenderer.addListener('directions_changed', function() {
-      computeTotalDistance(directionsRenderer.getDirections());
+  this.directionsRenderer.addListener('directions_changed', function () {
+    computeTotalDistance(this.directionsRenderer.getDirections());
   });
 
-  displayRoute('Egypt,cairo', 'Egypt,Alexandria', directionsService,
-      directionsRenderer);
+  originInput = document.getElementById("inputAddress");
+  destInput = document.getElementById("inputAddress2");
+  initOrigDestsAutocompelete();
+  // displayRoute();
+
 }
+var originInput;
+var destInput;
+var directionsService;
 
-function displayRoute(origin, destination, service, display) {
-  service.route({
-      origin: origin,
-      destination: destination,
-      waypoints: [{
-          location: new google.maps.LatLng(30.7865, 31.0004),
-      }, {
-          location: 'Egypt,damanhour'
-      }, {
-          location: 'Egypt,idku'
-      }],
-      travelMode: 'DRIVING',
-      avoidTolls: true
-  }, function(response, status) {
-      if (status === 'OK') {
-          display.setDirections(response);
-      } else {
-          alert('Could not display directions due to: ' + status);
-      }
+function displayRoute() {
+  this.directionsService.route({
+    origin: {
+      placeId: this.originPlaceId
+    },
+    waypoints: waypointPlaceIds.length?waypointPlaceIds:null,
+    destination: {
+      placeId: this.destinationPlaceId
+    }, // waypoints,
+    travelMode: 'DRIVING',
+    avoidTolls: false
+  }, function (response, status) {
+    if (status === 'OK') {
+      this.directionsRenderer.setDirections(response);
+    } else {
+      alert('Could not display directions due to: ' + status);
+    }
   });
 }
 
@@ -205,8 +210,133 @@ function computeTotalDistance(result) {
   var total = 0;
   var myroute = result.routes[0];
   for (var i = 0; i < myroute.legs.length; i++) {
-      total += myroute.legs[i].distance.value;
+    total += myroute.legs[i].distance.value;
   }
   total = total / 1000;
   document.getElementById('total').innerHTML = total + ' km';
 }
+
+function initWaypointsAutocompelete() {
+  for (let i = 0; i < waypointInputs.length; i++) {
+    const input = waypointInputs[i];
+    const inputAutocomplete = new google.maps.places.Autocomplete(input);
+    inputAutocomplete.setFields(["place_id"]);
+    inputAutocomplete.addListener("place_changed", () => {
+      const place = inputAutocomplete.getPlace();
+      waypointObj = {
+        placeId : place.place_id
+      }
+      waypointPlaceIds.push(waypointObj);
+      this.displayRoute();
+    });
+  }
+}
+
+
+function initOrigDestsAutocompelete() {
+  const originAutocomplete = new google.maps.places.Autocomplete(originInput);
+  originAutocomplete.setFields(["place_id"]);
+  setupPlaceChangedListener(originAutocomplete, "ORIG")
+
+  const destAutocomplete = new google.maps.places.Autocomplete(destInput);
+  destAutocomplete.setFields(["place_id"]);
+  setupPlaceChangedListener(destAutocomplete, "DEST")
+
+
+}
+
+var originPlaceId;
+
+var destinationPlaceId;
+
+function setupPlaceChangedListener(autocomplete, mode) {
+  autocomplete.bindTo("bounds", this.map);
+  autocomplete.addListener("place_changed", () => {
+    const place = autocomplete.getPlace();
+
+    if (!place.place_id) {
+      window.alert("Please select an option from the dropdown list.");
+      return;
+    }
+
+    if (mode === "ORIG") {
+      this.originPlaceId = place.place_id;
+    }
+    if (mode === "DEST") {
+      this.destinationPlaceId = place.place_id;
+    }
+    if (this.originPlaceId && this.destinationPlaceId) {
+      this.displayRoute();
+    }
+  });
+}
+
+// waypoint inputs logic
+var waypointID = 0;
+var waypointInputs = [];
+var waypointPlaceIds = [];
+
+function initAddWaypointButton() {
+  const btn = $("#add-waypoint");
+  btn.click(function (e) {
+    e.preventDefault();
+    console.log("clicked");
+    createWaypointInput();
+  });
+}
+
+function createWaypointInput() {
+  const originaInput = document.getElementById("inputAddress");
+  const newInput = document.createElement("input");
+  newInput.setAttribute("class", "form-control pickup-input waypoint_class");
+  newInput.setAttribute("id", "waypoint_" + waypointID);
+  newInput.setAttribute("placeholder", "Enter a waypoint");
+
+  const newLabel = document.createElement("label");
+  newLabel.setAttribute("for", "waypoint_" + waypointID);
+  newLabel.innerText = "Waypoint Location";
+
+  // create input container
+  const newInputContainer = document.createElement("div");
+  newInputContainer.setAttribute("class", "inputAdress-waypoint");
+  newInputContainer.setAttribute("id", "waypoint_container_" + waypointID);
+
+
+  // add waypoint button
+  const newbtn = document.createElement("button");
+  newbtn.setAttribute("class", "pickup-button");
+  newbtn.innerText = "+";
+  $(newbtn).click(function (e) {
+    e.preventDefault();
+    createWaypointInput();
+  });
+
+  // remove waypoint button
+  const deleteWatpoint = document.createElement("button");
+  deleteWatpoint.setAttribute("class", "pickup-button delete-waypoint");
+  deleteWatpoint.innerText = "-";
+
+  $(deleteWatpoint).click(function (e) {
+    e.preventDefault();
+    newInputContainer.remove();
+    let removed = waypointInputs.indexOf(newInput);
+    for (var i = 0; i < waypointInputs.length; i++) {
+      if (waypointInputs[i] === newInput) {
+        waypointInputs.splice(i, 1);
+        break;
+      }
+    }
+  });
+
+
+  originaInput.parentElement.parentElement.append(newInputContainer);
+  newInputContainer.append(newLabel);
+  newInputContainer.append(newInput);
+  newInputContainer.append(deleteWatpoint);
+  newInputContainer.append(newbtn);
+
+  waypointID++;
+  waypointInputs.push(newInput);
+  initWaypointsAutocompelete();
+}
+initAddWaypointButton();
